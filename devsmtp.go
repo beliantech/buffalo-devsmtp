3f8e28f2386b905d570f67930336c02804e76863
Jonathan Lin <jonathan@beliantech.com>
2021-11-09 16:30:31 +0800

    Initial commit

---
+++ devsmtp.go
@@ -0,0 +1,40 @@
+package devsmtp
+
+import (
+	"io/ioutil"
+
+	"github.com/ernsheong/grand"
+	"github.com/gobuffalo/buffalo/mail"
+	"github.com/pkg/browser"
+)
+
+// DevSMTP is an in-memory implementation for buffalo `Sender`
+// interface. It's intended to open sent messages in the browser for debugging purposes.
+type DevSMTP struct {
+}
+
+// Send implements buffalo `Sender` interface, to open the mail in the browser.
+func (s *DevSMTP) Send(m mail.Message) error {
+	filehash := grand.GenerateRandomString(16)
+	previewFilename := "tmp/preview_" + filehash + ".html"
+
+	// Only care about HTML for now
+	htmlContent := ""
+	for _, body := range m.Bodies {
+		if body.ContentType == "text/html" {
+			htmlContent += body.Content
+		}
+	}
+
+	if err := ioutil.WriteFile(previewFilename, []byte(htmlContent), 0644); err != nil {
+		return err
+	}
+
+	defer browser.OpenFile(previewFilename)
+	return nil
+}
+
+// New constructs a new DevSMTP.
+func New() *DevSMTP {
+	return &DevSMTP{}
+}
